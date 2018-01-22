f6f51e957f67392043aa304e1b6a6d2f7b3110a0
R-Canai <canai3jp@gmail.com>
2018-01-22 21:45:41 +0900

    ignore再設定

---
+++ assets/js/pages/home.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],[,function(o,c,e){"use strict";console.log("home !")}],[1]);
---
+++ dev-assets/scripts/application.js
@@ -0,0 +1,12 @@
+class Hoge {
+  constructor() {
+    this.name = 'Hoge';
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+const hoge = new Hoge();
+console.log(hoge.getName());
